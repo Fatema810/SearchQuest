ae5c016b07d4a76ef66a9b9791bf4aab84e81875
FatemaHussain08 <102172078+FatemaHussain08@users.noreply.github.com>
2023-07-06 23:31:54 -0400

    Comments added

--- backend/server.py
+++ backend/server.py
@@ -4,13 +4,17 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Set up your Google Custom Search Engine ID and API key
+
+#Google Custom Search Engine ID
 engine_id = '02545075c61a24321'
+
+#Google API key
 api_key = 'AIzaSyChfaN-id-IAkInxkPoLwx49YfHgF4krsY'
 
-# Initialize the question-answering pipeline
+# question-answering pipeline initialized
 pipeline_search = pipeline("question-answering", model="distilbert-base-cased-distilled-squad")
 
+#Path that accepts requests
 @app.route('/searchQuery', methods=['POST'])
 def perform_search():
     data = request.get_json()
@@ -27,32 +31,21 @@ def perform_search():
     return jsonify(response)
 
 
-
+#Method that answers questions to given topic using distilbert-base-cased-distilled-squad model
 def result_questions(results, question):
     if results:
-        # Extract the page content from the search results
-        # page_content = ' '.join(result['snippet'] for result in results)
-
-
-        # Assuming 'results' is a list of dictionaries containing search results
-# and each dictionary has a 'snippet' key
-
-# Create an empty list to store the snippets
-        snippets_dic = []
+#Empty list created to store snippets 
+        snippets_lst = []
 
 # Iterate over each result in the 'results' list
         for result in results:
-    # Access the 'snippet' key from the current result dictionary
-            snippet_dic = result['snippet']
-    
-    # Append the snippet to the 'snippets' list
-            snippets_dic.append(snippet_dic)
+    # Access the 'snippet' key
+            snippet_kst = result['snippet']
+            snippets_lst.append(snippets_lst)
 
-# Join the snippets in the 'snippets' list using a space separator
-        page_content = ' '.join(snippets_dic)
+        page_content = ' '.join(snippets_lst)
 
-
-        # Perform question-answering using the Hugging Face model
+        #  question-answering using the Hugging Face model
         answer = pipeline_search(question=question, context=page_content)
 
         return answer['answer']
@@ -60,24 +53,18 @@ def result_questions(results, question):
         return "No search results found."
 
 
-# Function to search Google using the Custom Search API
+# Function to search Google for given topic
 def search_internet(topic, question):
     # Make a request to the Google Custom Search API
     url = f'https://www.googleapis.com/customsearch/v1?key={api_key}&cx={engine_id}&q={topic}+{question}'
     res = requests.get(url)
     res.raise_for_status()
 
-    # Extract the search results from the response
+    # Extract the search results
     data = res.json()
     results = data.get('items', [])
 
     return results
-
-# Function to answer questions related to the search results
-
-
-
-
 
 
 if __name__ == '__main__':
